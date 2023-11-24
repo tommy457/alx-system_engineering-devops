a77667c8cf8fff833d8bbc9b977c95988465800f
tommy457 <boukhalfaml1011@gmail.com>
2023-11-24 09:35:09 -0800

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+file { 'create file':
+  ensure  => 'file',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
