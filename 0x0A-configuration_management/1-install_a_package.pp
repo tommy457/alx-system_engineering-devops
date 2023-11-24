408064d152e2c6391a6ec3701e3e3b1a6ecfe543
tommy457 <boukhalfaml1011@gmail.com>
2023-11-24 10:46:44 -0800

    task 1 bug fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 # install flask from pip3.
+package { 'Werkzeug':
+  ensure   => '2.2.2',
+  provider => 'pip3',
+}
 
-package { 'flask':
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
