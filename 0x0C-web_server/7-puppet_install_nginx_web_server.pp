e5e9475110f472b7c990ea5b2b64f4334c1e8efa
tommy457 <boukhalfaml1011@gmail.com>
2023-11-28 11:42:55 -0800

    puppt congi nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,37 @@
+# configuring a NGINX web server
+
+exec { 'update':
+  path    => '/usr/bin:/bin',
+  command => 'apt-get update',
+}
+
+exec { 'install nginx':
+  path    => '/usr/bin:/bin',
+  command => 'apt-get install nginx -y',
+}
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  content => "Hello World!\n",
+}
+
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
+}
+
+file_line { '404':
+  path  => '/etc/nginx/sites-enabled/default',
+  after => 'server_name _;',
+  line  => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file_line { 'redirect':
+  path  => '/etc/nginx/sites-enabled/default',
+  after => 'server_name _;',
+  line  => '        error_page 404 /custom_404.html;',
+}
+
+exec {'/usr/bin/env service nginx restart':}
