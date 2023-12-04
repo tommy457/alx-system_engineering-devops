309d8c57186f1f7c51bf4af7f2b1627dcb2ec097
tommy457 <boukhalfaml1011@gmail.com>
2023-12-04 05:32:24 -0800

    remove rederect and 404

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,42 +2,22 @@
 
 exec { 'update':
   path    => '/usr/bin:/bin',
-  command => 'apt-get update',
+  command => 'sudo apt-get update',
 }
 
 exec { 'install nginx':
   path    => '/usr/bin:/bin',
-  command => 'apt-get install nginx -y',
+  command => 'sudo apt-get install nginx -y',
 }
 
 package { 'nginx':
   ensure => 'installed',
 }
 
-file { '/var/www/html/index.nginx-debian.html':
-  content => "Hello World!\n",
-}
-
-file { '/var/www/html/404.html':
-  content => "Ceci n'est pas une page\n",
-}
-
-file_line { '404':
+file_line { 'add header':
   path  => '/etc/nginx/sites-enabled/default',
   after => 'server_name _;',
-  line  => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line  => "        add_header X-Served-By ${hostname} always;",
 }
 
-file_line { 'redirect':
-  path  => '/etc/nginx/sites-enabled/default',
-  after => 'server_name _;',
-  line  => '        error_page 404 /custom_404.html;',
-}
-
-file_line { 'header':
-  path  => '/etc/nginx/sites-enabled/default',
-  after => 'server_name _;',
-  line  => '        add_header X-Served-By $hostname always;',
-}
-
-exec {'/usr/bin/env service nginx restart':}
+exec {'/usr/bin/env sudo service nginx restart':}
