6ac89ecb95903fef1872b1e52f9fc1dcaab6e2de
tommy457 <boukhalfaml1011@gmail.com>
2023-12-05 04:47:53 -0800

    ordering

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,22 +5,22 @@ exec { 'update':
   command => 'sudo apt-get update',
 }
 
-exec { 'install nginx':
+-> exec { 'install nginx':
   path    => '/usr/bin:/bin',
   command => 'sudo apt-get install nginx -y',
 }
 
-package { 'nginx':
+-> package { 'nginx':
   ensure => 'installed',
 }
 
-file_line { 'add header':
+-> file_line { 'add header':
   path  => '/etc/nginx/sites-available/default',
   after => 'server_name _;',
   line  => '        add_header X-Served-By $hostname;',
 }
 
-exec { 'restart nginx':
+-> exec { 'restart nginx':
   path    => '/usr/bin:bin/',
   command => 'sudo service nginx restart',
 }
