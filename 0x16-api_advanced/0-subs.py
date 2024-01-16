a1d04a08e55ab486acd0475e474acbe348d8c87c
tommy457 <boukhalfaml1011@gmail.com>
2024-01-16 11:18:55 -0800

    check for None input

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,13 @@ import requests
 def number_of_subscribers(subreddit):
     """Function that queries the Reddit API,
     returns the number of subscribers"""
-    url = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
+    if subreddit is None:
+        return 0
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Firefox/120.0'}
 
     res = requests.get(url=url, headers=headers, allow_redirects=False)
     if res.status_code == 200:
-        data = res.json().get("data")
-        return data.get("subscribers")
+        data = res.json().get("data", {})
+        return data.get("subscribers", 0)
     return 0
